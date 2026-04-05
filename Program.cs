a448363d20fde93dd92ac1514c15e38b12c3a231
Basiliksys <belloslav@gmail.com>
2026-04-05 13:11:00 +0300

    Added Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+class Program
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    static void Main(string[] args)
+    {
+        // Creating products (Agregation)
+        var myProducts = new List<Product>
+        {
+            new Product("Milk", DateTime.Now.AddDays(-1)),
+            new Product("Cheese", DateTime.Now.AddDays(5))
+        };
+
+        // Initializing fridge
+        SmartFridge fridge = new SmartFridge(myProducts);
+
+        // Events initializing
+        fridge.OnAlert += (msg) => Console.WriteLine($"[Alert]: {msg}");
+        fridge.OnTemperatureChanged += (msg) => Console.WriteLine($"[Telemetry]: {msg}");
+
+        try
+        {
+            fridge.ExecuteVoiceCommand("Product status?");
+            for (int i = 0; i < 5; i++)
+            {
+                fridge.Work();
+            }
+        }
+        catch (SmartDeviceException ex)
+        {
+            Console.WriteLine($"Components error ({ex.ErrorTime}): {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"System error: {ex.Message}");
+        }
+
+        Console.WriteLine("\nWork with conditioner:");
+        SmartAirConditioner ac = new SmartAirConditioner();
+        ac.StartCooling();
+    }
 }
--- SmartAirConditioner.cs
+++ SmartAirConditioner.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 
-public class Class1
+public class SmartAirConditioner : SmartFridge
 {
-	public Class1()
-	{
-	}
+    private bool filterCleane { get; set; } = true;
+    
+    public void StartCooling()
+    {
+        Console.WriteLine("SmartAirConditioner is cooling...");
+        if (!filterCleane)
+            throw new SmartDeviceException("Air filter is dirty! Please clean it.");
+        
+        // Simulate cooling process
+        Random random = new Random();
+        if (random.Next(0, 10) == 5)
+            throw new SmartDeviceException("Critical cooling system error!");
+        
+        Console.WriteLine("Cooling successful. Current temperature: " + targetTemperature + "°C");
+    }
 }
